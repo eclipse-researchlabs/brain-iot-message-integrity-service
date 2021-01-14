c56eb749df9c1fe0612bb6689528121f3186fd29
Tim Ward <timothyjward@apache.org>
2021-01-14 15:46:42 +0000

    Add EPLv2 License and copyright headers

--- message.integrity.api/src/main/java/eu/brain/iot/eventing/message/integrity/api/package-info.java
+++ message.integrity.api/src/main/java/eu/brain/iot/eventing/message/integrity/api/package-info.java
@@ -1,7 +1,12 @@
-/* Copyright 2020 Paremus, Ltd - All Rights Reserved
- * Unauthorized copying of this file, via any medium is strictly prohibited
- * Proprietary and confidential
- */
+/*******************************************************************************
+ * Copyright (C) 2021 Paremus
+ * 
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ ******************************************************************************/
 
 @org.osgi.annotation.bundle.Export
 @org.osgi.annotation.versioning.Version("0.0.1")
